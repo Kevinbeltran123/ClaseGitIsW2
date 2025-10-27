9a736938ac716afad868f80262ada828327a6141
KevinBel <kevin.beltran@estudiantesunibague.edu.co>
2025-10-27 13:30:13 -0500

    Nuevos cambios en controller

--- controllers/comicsController.js
+++ controllers/comicsController.js
@@ -1,10 +1,23 @@
+const axios = require('axios');
 const comicsController = {};
 
 comicsController.getComics = (req, res) => {
-  res.json({
-    message: 'Lista de comics',
-    comics: []
-  });
+  const APIkey = process.env.APIKEY;
+  const TS = process.env.TS;
+  const HASH = process.env.HASH;
+  const MARVEL_API = process.env.MARVEL_API;
+
+  axios.get(`${MARVEL_API}?ts=${TS}&apikey=${APIkey}&hash=${HASH}`)
+    .then(response => {
+      res.json({
+        message: 'Lista de comics',
+        comics: response.data.data.results
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching comics:', error);
+    });
 };
 
 module.exports = comicsController;
+
